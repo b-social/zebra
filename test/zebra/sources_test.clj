fb8904e0afe0121c4ae1361d222f7cd206c3d2a8
Jonas Svalin <jonas.svalin@b-social.com>
2018-11-05 13:23:46 +0000

    Add test for creating 3d secure source

--- test/zebra/sources_test.clj
+++ test/zebra/sources_test.clj
@@ -10,6 +10,22 @@
       (is (some? (:id source)))
       (is (map? (:type-data source))))))
 
+(deftest create-three-d-secure-source
+  (let [card-source (sources/create
+                      {:type  "card"
+                       :token (:valid-token tokens)}
+                      api-key)
+        three-d-secure-source (sources/create
+                                {:type           "three_d_secure"
+                                 :three_d_secure {:card (:id card-source)}
+                                 :redirect {:return_url "https://shop.example.com/crtA6B28E1"}
+                                 :amount         100
+                                 :currency       "gbp"}
+                                api-key)]
+    (testing "should be a valid source"
+      (is (some? (:id three-d-secure-source)))
+      (is (map? (:type-data three-d-secure-source))))))
+
 (deftest retrieve-source
   (let [source (sources/create {:type  "card"
                                 :token (:valid-token tokens)} api-key)
