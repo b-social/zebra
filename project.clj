448860d88173471cdf3d44545099ce9a336b6fb2
Jonas Svalin <jonas.svalin@b-social.com>
2019-09-23 14:33:23 +0100

    Version 0.1.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/zebra "0.1.8"
+(defproject b-social/zebra "0.1.9-SNAPSHOT"
   :description "A clojure wrapper for Stripe"
   :url "https://github.com/b-social/zebra"
   :license {:name "The MIT License"
